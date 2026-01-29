1ff5eddb7213b2f9a05c6c933567e5866e8e9668
Sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-01-29 11:46:46 +0530

    Add production URL to CORS allowed origins

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174'
+  'http://127.0.0.1:5174',
+  'https://course-platform-cyan.vercel.app',
 ].filter(Boolean);
 
 app.use(cors({
